38354e8d086270ad422edaa4f99a4ae19dcf78d1
Aditya Singh <thakur.11913421@lpu.in>
2022-03-22 02:03:20 +0530

    33march

---
+++ javascript/calculations.js
@@ -0,0 +1,24 @@
+var sum = 10 + 0;
+console.log(sum);
+
+var diff = 45 -33;
+console.log(sum);
+
+var multiply= 45*2;
+console.log(multiply);
+
+var quitionent  = 66/33;
+console.log(quitionent);
+
+// incrementing a number  
+myVar= myVar + 100;
+// or 
+myVar++;
+myVar--;
+
+// Dividing decimal numbers 
+var quitionent = 2.2/5
+
+// REMAINDER OPERATOR IS OFTENLY USED TO MAKE SURE WEATHER THE NUMBER IS ODD OR EVENT 
+
+
---
+++ javascript/datatypes_variables.js
@@ -0,0 +1,26 @@
+// There are different dayatypes in Javascript:
+// - undefined, null, Boolean, String, Symbol, Number and Object. 
+
+/*
+
+- We can use the semi colon at the end or even dont use it, it is fine... 
+
+*/ 
+
+// Ways of declaring a variable in JS 
+
+var name = "AdityaSingh"
+let ournamer = "TADS"
+const pi = 3.14
+
+// Storing values with asignment operation. 
+
+var a; // declaring the variable 
+var b = 2; // declaring and assigning to b 
+
+a = 7; // assigning the value oth variable a 
+b = a; 
+
+console.log(a)
+
+
